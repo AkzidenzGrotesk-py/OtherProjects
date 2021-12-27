6bfcb780c8c7fc8f7b84dd5e9302fe8cf820f0b2
Sky Coleman <72958909+AkzidenzGrotesk-py@users.noreply.github.com>
2021-12-26 16:11:20 -0800

    Update cmdmake.c

--- c/cmdmake.c
+++ c/cmdmake.c
@@ -67,3 +67,75 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
+/*
+ALTERNATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Takes file 'file.c' and converts to 'file.o'
+char *dotc_doto(char *filename) {
+  int sz = (int)strlen(filename);
+  char *copy = malloc((sz+1) * sizeof(char));
+  strcpy_s(copy, (sz+1) * sizeof(char), filename);
+  copy[sz-1] = 'o';
+  return copy;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("\033[31marguments required:\033[0m executable_name and files...\nexample: foundation hello hello.c\n");
+    exit(1);
+  }
+
+  char *exec_name = argv[1];
+  char *standard_opts = "-Wall -Wextra -pedantic -std=c11 -funsigned-char -Werror=implicit-function-declaration -Werror=format-security";
+
+  printf("Attempting to open file 'build.cmd'");
+  FILE *outfile;
+  if (fopen_s(&outfile, "build.cmd", "w") != 0) {
+    printf(",\033[31m failed.\033[0m\n");
+    exit(1);
+  }
+  printf(", done.\n");
+  fprintf_s(outfile, "@echo off\n");
+  if (argc == 3) {
+    // Single file, skipping compilation to object file
+    printf("Generating line for %s -> %s.exe", argv[2], exec_name);
+    fprintf_s(outfile, "echo (@) - Compiling \033[33m%s\033[0m -^> %s.exe\n",
+              argv[2], exec_name);
+    fprintf_s(outfile, "gcc %s -o %s %s\n",
+              argv[2], exec_name, standard_opts);
+    printf(", done.\n");
+  } else {
+    // Multiple files, compile to object and link
+    char *objn[argc-2];
+    for (int i = 2; i < argc; i++) {
+      printf("Generating line for %s -> %s", argv[i], dotc_doto(argv[i]));
+      fprintf_s(outfile, "echo (%i) - Compiling \033[33m%s\033[0m -^> %s\n",
+          i - 2, argv[i], dotc_doto(argv[i]));
+      fprintf_s(outfile, "gcc %s -c %s\n",
+                standard_opts, argv[i]);
+      objn[i - 2] = dotc_doto(argv[i]);
+      printf(", done.\n");
+    }
+    printf("Generating linkage line -> %s.exe", exec_name);
+    fprintf_s(outfile, "echo (@) - Linking \033[33m");
+    for (int i = 0; i < argc - 2; i++)
+      fprintf_s(outfile, "%s, ", objn[i]);
+    fprintf_s(outfile, "\033[0m -^> %s.exe\n",
+              exec_name);
+    fprintf_s(outfile, "gcc ");
+    for (int i = 0; i < argc - 2; i++)
+      fprintf_s(outfile, "%s ", objn[i]);
+    fprintf_s(outfile, "-o %s\n",
+              exec_name);
+    printf(", done.\n");
+  }
+  fclose(outfile);
+  printf("Completed.\n");
+
+  return 0;
+}
+*/
